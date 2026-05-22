dd843c6854cfc8657411ef96801cefe88b354bfb
Moacyr Pinheiro <moacyr.pinheiro@ibrowse.net.br>
2026-05-22 17:35:19 -0300

    wip

--- AnimePomodoro/wwwroot/js/site.js
+++ AnimePomodoro/wwwroot/js/site.js
@@ -1,1 +1,20 @@
 ﻿setTitle = (title) => { document.title = title; };
+
+window.lofi = {
+    player: null,
+    init: function (volume) {
+        if (window.lofi.player) return;
+        window.lofi.player = new YT.Player('yt-player', {
+            videoId: 'jfKfPfyJRdk',
+            playerVars: { autoplay: 0, controls: 0, fs: 0, rel: 0, iv_load_policy: 3 },
+            events: {
+                onReady: function () {
+                    window.lofi.player.setVolume(volume);
+                }
+            }
+        });
+    },
+    play: function () { if (window.lofi.player) window.lofi.player.playVideo(); },
+    pause: function () { if (window.lofi.player) window.lofi.player.pauseVideo(); },
+    setVolume: function (vol) { if (window.lofi.player) window.lofi.player.setVolume(vol); }
+};
